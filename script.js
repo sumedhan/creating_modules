26f95fb00563baf7cad470818bf5fa0d54f7b58f
sumedha <sumedhanarayanan@gmail.com>
2019-01-11 21:01:04 +0000

    Completes script.js. Adds a few numbers to the list and then sorts it.

---
+++ script.js
@@ -0,0 +1,14 @@
+var dataList = require("./dataList.js");
+
+dataList.saveToList(3);
+dataList.saveToList(14);
+dataList.saveToList(42);
+dataList.saveToList(312);
+dataList.saveToList(1);
+dataList.saveToList(13);
+dataList.saveToList(34);
+dataList.saveToList(3424);
+git dataList.showList();
+
+var list2 = dataList.sortListAscending();
+console.log(list2);
